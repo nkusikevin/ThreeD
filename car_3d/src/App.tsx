aa79d9af649d8d46bd08126973f084efe00592ca
nkusikevin <nkusikvnhart@gmail.com>
2023-02-24 17:57:36 +0200

    adding env cube

--- car_3d/src/App.tsx
+++ car_3d/src/App.tsx
@@ -1,35 +1,56 @@
 import { Canvas } from "@react-three/fiber";
-import { Center, useGLTF } from "@react-three/drei";
-import { OrbitControls, RoundedBox } from "@react-three/drei";
+import {
+	Center,
+	Environment,
+	useGLTF,
+	OrbitControls,
+	RoundedBox,
+	Stage,
+} from "@react-three/drei";
+
 import Model from "./Model";
 import { Effects } from "./Effect";
 function App() {
 	return (
 		<div className='bg'>
 			<Canvas>
+				<Environment preset='night' />
 				<ambientLight intensity={0.5} />
 				<pointLight position={[10, 10, 10]} />
 
 				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-				<OrbitControls />
+				<OrbitControls
+					autoRotate
+					enablePan={false}
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
 				<Center>
-					<Model />
+					<Stage
+						environment={"night"}
+						intensity={1}
+						// contactShadow={false}
+						// shadowBias={-0.0015}
+					>
+						<Model />
+					</Stage>
 
-					<RoundedBox
+					{/* <RoundedBox
 						receiveShadow
 						castShadow
 						smoothness={10}
 						radius={0.2}
 						rotation-x={-Math.PI / 2}
 						position-z={3}
-						scale={[8, 10, 0.2]}>
+						scale={[6, 6, 0.2]}>
 						<meshStandardMaterial
-							color='skyblue'
+							color='rgb(35, 36, 39) 99%'
 							envMapIntensity={0.5}
 							roughness={0}
 							metalness={0}
 						/>
-					</RoundedBox>
+					</RoundedBox> */}
 				</Center>
 				{/* <Effects /> */}
 			</Canvas>
--- car_3d/src/Model.tsx
+++ car_3d/src/Model.tsx
@@ -1,11 +1,72 @@
 import React from "react";
+import { useLayoutEffect } from "react";
 import { Center, useGLTF } from "@react-three/drei";
+import { applyProps } from "@react-three/fiber";
+import * as THREE from "three";
 
 function Model() {
-	const { materials, node, scene }: any = useGLTF("/jeep.glb");
-	console.log(materials);
+	const { materials, nodes, scene }: any = useGLTF("/jeep.glb");
 
-	return <primitive object={scene} scale={1} position={[0, -0.2, 3]} />;
+	useLayoutEffect(() => {
+		Object.values(nodes).forEach(
+			(node: any) =>
+				node.isMesh && (node.receiveShadow = node.castShadow = true)
+		);
+		applyProps(materials.Glass, {
+			color: "white",
+			roughness: 0,
+			clearcoat: 0.1,
+		});
+		applyProps(materials.Main_Paint, {
+			envMapIntensity: 2,
+			roughness: 0.45,
+			metalness: 0.8,
+			color: "#888",
+		});
+		applyProps(materials.Rims, {
+			color: "#474747",
+			envMapIntensity: 2,
+			roughness: 0.45,
+			metalness: 0.8,
+		});
+		applyProps(materials.Chrome, {
+			color: "white",
+			envMapIntensity: 2,
+			roughness: 0,
+			clearcoat: 0.1,
+		});
+		applyProps(materials.Indicators, {
+			color: "red",
+			envMapIntensity: 2,
+			roughness: 0,
+			clearcoat: 0.1,
+		});
+		applyProps(materials.Tyres, {
+			color: "black",
+			envMapIntensity: 2,
+			roughness: 0,
+			clearcoat: 0.1,
+		});
+		applyProps(materials.Mirrors, {
+			color: "white",
+			roughness: 0,
+			clearcoat: 0.1,
+		});
+
+		applyProps(materials.material_0, {
+			color: "red",
+			envMapIntensity: 2,
+			roughness: 0.45,
+			metalness: 0.8,
+		});
+		applyProps(materials.Suspension, {
+			color: "red",
+			envMapIntensity: 2,
+			roughness: 0.45,
+			metalness: 0.8,
+		});
+	}, [nodes, materials]);
+	return <primitive object={scene} scale={1} position={[0, -0.17, 3]} />;
 }
 
 export default Model;
