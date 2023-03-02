1a8530b95d25aa9c06aaaea113aaf836a5758c00
nkusikevin <nkusikvnhart@gmail.com>
2023-03-02 21:52:11 +0200

    adding side words

--- threeDhero/src/App.tsx
+++ threeDhero/src/App.tsx
@@ -7,15 +7,17 @@ function App() {
 		<div className='bg'>
 			<Navigation />
 
-			<div className='mt-20 px-10 w-6/12'>
-				<h1 className='text-9xl font-bold font-mono'>Work</h1>
-				<p className='text-4xl font-mono font-semibold'>Pixel as nature</p>
-				<p className='mt-4 text-lg font-medium font-mono'>
-					we moved past the myth that our world is flat - but we're still
-					designing in blocks and flat color . As our digital interface start to
-					overlay the world and our realities mix digital design in final living
-					in three-dimensions
-				</p>
+			<div className='flex flex-row items-center'>
+				<div className=' px-10 w-6/12'>
+					<h1 className='text-9xl font-bold font-mono'>Work</h1>
+					<p className='text-4xl font-mono font-semibold'>Pixel as nature</p>
+					<p className='mt-4 text-lg font-medium font-mono'>
+						we moved past the myth that our world is flat - but we're still
+						designing in blocks and flat color . As our digital interface start
+						to overlay the world and our realities mix digital design in final
+						living in three-dimensions
+					</p>
+				</div>
 			</div>
 		</div>
 	);
