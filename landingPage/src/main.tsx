b7184d0b482c0748c32690d8cc1f526cbc93ac4f
nkusikevin <nkusikvnhart@gmail.com>
2023-03-15 22:07:25 +0200

    adding assets

--- landingPage/src/main.tsx
+++ landingPage/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import logo from "./assets/logo-2.png";
+import { Canvas } from "@react-three/fiber";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<header className='App-header'>
+			<img src={logo} className='App-logo' alt='logo' />
+		</header>
+		<Canvas shadows>
+			<App />
+		</Canvas>
+
+		<button className='btn--sound'>
+			<span className='material-symbols-rounded'>music_note</span>
+		</button>
+	</React.StrictMode>
+);
