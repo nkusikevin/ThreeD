c07e2dcb523d6341ee9dd0cb6b7a5b18ceef4ffa
nkusikevin <nkusikvnhart@gmail.com>
2023-03-16 22:10:16 +0200

    adding the first row

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -15,7 +15,24 @@ function App() {
 				<Scroll></Scroll>
 
 				<Scroll html style={{ width: "100%" }}>
-					<Container style={{ position: "relative" }}></Container>
+					<Container style={{ height: "100px", position: "relative" }}>
+						<Row
+							className='text-center align-items-center justify-content-center'
+							style={{
+								position: "absolute",
+								width: "100%",
+								height: "100vh",
+								padding: "0px 30px 0px",
+							}}>
+							<Col xs={6}>
+								<div>
+									<h1 style={{ marginBottom: "0px" }}>
+										Life can be a struggle
+									</h1>
+								</div>
+							</Col>
+						</Row>
+					</Container>
 				</Scroll>
 			</ScrollControls>
 		</>
