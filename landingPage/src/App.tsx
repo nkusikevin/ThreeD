36f23c3c20716ab11cf022e15ab37c5c25ef6661
nkusikevin <nkusikvnhart@gmail.com>
2023-03-16 22:11:00 +0200

    adding the second row

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -32,6 +32,23 @@ function App() {
 								</div>
 							</Col>
 						</Row>
+						<Row
+							className='text-center align-items-center justify-content-center'
+							style={{
+								position: "absolute",
+								width: "100%",
+								height: "100vh",
+								padding: "0px 30px 0px",
+								top: "100vh",
+							}}>
+							<Col xs={6}>
+								<div>
+									<h1 style={{ marginBottom: "0px" }}>
+										Sometimes you can feel
+									</h1>
+								</div>
+							</Col>
+						</Row>
 					</Container>
 				</Scroll>
 			</ScrollControls>
