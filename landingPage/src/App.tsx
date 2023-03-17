321d73ddb5477dbe26fc2013ed733e9a9537bf2d
nkusikevin <nkusikvnhart@gmail.com>
2023-03-17 22:12:33 +0200

    adding two last rows

--- landingPage/src/App.tsx
+++ landingPage/src/App.tsx
@@ -84,6 +84,49 @@ function App() {
 								</div>
 							</Col>
 						</Row>
+						<Row
+							className='text-center align-items-center justify-content-center'
+							style={{
+								position: "absolute",
+								width: "100%",
+								height: "100vh",
+								padding: "0px 30px 0px",
+								top: "400vh",
+							}}>
+							<Col xs={8}>
+								<div>
+									<h1 style={{ marginBottom: "0px" }}>
+										But there is hope...
+										<br /> and people who can help
+									</h1>
+								</div>
+							</Col>
+						</Row>
+
+						<Row
+							className='text-center align-items-center justify-content-center'
+							style={{
+								position: "absolute",
+								width: "100%",
+								height: "100vh",
+								padding: "0px 30px 0px",
+								top: "500vh",
+							}}>
+							<Col xs={6}>
+								<div>
+									<h1 style={{ marginBottom: "0px" }}>
+										It's time to get
+										<br /> the support you need
+									</h1>
+									<h2 style={{ marginBottom: "30px", marginTop: "-20px" }}>
+										To get your life back
+									</h2>
+									<Button variant='outline-light' size='lg'>
+										Get help now
+									</Button>{" "}
+								</div>
+							</Col>
+						</Row>
 					</Container>
 				</Scroll>
 			</ScrollControls>
