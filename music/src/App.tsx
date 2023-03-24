c68e8ee09faa8a92abde0055d63b901798e45072
nkusikevin <nkusikvnhart@gmail.com>
2023-03-24 20:52:55 +0200

    adding music copm

--- music/src/App.tsx
+++ music/src/App.tsx
@@ -1,32 +1,29 @@
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
-
-import {
-	Center,
-	Environment,
-	OrbitControls,
-	PerspectiveCamera,
-	Stars,
-} from "@react-three/drei";
+import Music from "./components/Music";
+import { Center, Environment, OrbitControls } from "@react-three/drei";
 import Mode from "./components/Mode";
 
 function App() {
 	return (
-		<Canvas>
-			<ambientLight />
-			<pointLight position={[10, 10, 10]} />
-			<OrbitControls
-				autoRotate
-				enablePan={false}
-				enableZoom={false}
-				maxPolarAngle={Math.PI / 2}
-				minPolarAngle={Math.PI / 2}
-			/>
-			<Center>
-				<Mode />
-			</Center>
-			<Environment preset='city' />
-		</Canvas>
+		<>
+			<Canvas>
+				<ambientLight />
+				<pointLight position={[10, 10, 10]} />
+				<OrbitControls
+					autoRotate
+					enablePan={false}
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Center>
+					<Mode />
+				</Center>
+				<Environment preset='city' />
+			</Canvas>
+			<Music />
+		</>
 	);
 }
 
---
+++ music/src/components/Music.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Music() {
+	return <div>Music</div>;
+}
+
+export default Music;
