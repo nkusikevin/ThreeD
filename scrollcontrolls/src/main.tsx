1456e637caa3dd7cf520d3e0f27056038587a7f7
nkusikevin <nkusikvnhart@gmail.com>
2023-05-04 18:54:30 +0200

    init scroll controls app

---
+++ scrollcontrolls/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<Canvas>
+			<App />
+		</Canvas>
+	</React.StrictMode>
+);
