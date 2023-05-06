e4c75625e98bdd8a692570594440c6ad1cca5a07
nkusikevin <nkusikvnhart@gmail.com>
2023-05-06 20:19:58 +0200

    adding model

--- scrollcontrolls/src/App.tsx
+++ scrollcontrolls/src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from "@react-three/drei";
 //@ts-ignore
 import baffle from "baffle";
+import Robot from "./components/Robot";
 
 function App() {
 	return (
@@ -26,6 +27,73 @@ function App() {
 				shadow-bias={-0.0001}
 			/>
 			<Environment preset='warehouse' />
+			<ScrollControls pages={3} damping={0.1}>
+				<Scroll>
+					<Robot />
+				</Scroll>
+				<Scroll html style={{ width: "100%" }}>
+					<h1
+						className='title'
+						style={{
+							color: "#cdcbca",
+							position: "absolute",
+							top: `65vh`,
+							left: "50%",
+							fontSize: "13em",
+							transform: `translate(-50%,-50%)`,
+						}}>
+						PHANTOM
+					</h1>
+
+					<div className='row' style={{ position: "absolute", top: `132vh` }}>
+						<h2>Be a Man of the Future.</h2>
+						<p style={{ maxWidth: "400px" }}>
+							Featuring a sleek, metallic design inspired by advanced
+							technology, this aftershave bottle is as stylish as it is
+							functional. But it's not just a pretty face - inside, you'll find
+							a nourishing and protective aftershave formula that will leave
+							your skin feeling refreshed and hydrated.
+						</p>
+						<button>Read more</button>
+					</div>
+
+					<div className='row' style={{ position: "absolute", top: `230vh` }}>
+						<div
+							className='col'
+							style={{ position: "absolute", right: `40px`, width: "540px" }}>
+							<h2 style={{ maxWidth: "440px" }}>Tech-Savvy Side</h2>
+							<p style={{ maxWidth: "440px" }}>
+								Featuring a sleek, metallic design inspired by advanced
+								technology, this aftershave bottle is as stylish as it is
+								functional. But it's not just a pretty face - inside, you'll
+								find a nourishing and protective aftershave formula that will
+								leave your skin feeling refreshed and hydrated.
+							</p>
+							<button>Read more</button>
+						</div>
+					</div>
+
+					<h2
+						style={{
+							position: "absolute",
+							top: "350vh",
+							left: "50%",
+							transform: `translate(-50%,-50%)`,
+						}}>
+						Cutting-Edge of Grooming
+					</h2>
+
+					<button
+						style={{
+							position: "absolute",
+							top: `590vh`,
+							left: "50%",
+							transform: `translate(-50%,-50%)`,
+						}}>
+						Buy now
+					</button>
+				</Scroll>
+			</ScrollControls>
 		</>
 	);
 }
