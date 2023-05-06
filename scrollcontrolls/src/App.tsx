423c482da6184ae0858e7dcea055d243c232584d
nkusikevin <nkusikvnhart@gmail.com>
2023-05-06 19:04:05 +0200

    init application

--- scrollcontrolls/src/App.tsx
+++ scrollcontrolls/src/App.tsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import { useEffect } from "react";
+import {
+	RoundedBox,
+	ScrollControls,
+	Scroll,
+	Environment,
+	Sparkles,
+	Backdrop,
+	Float,
+	Ring,
+} from "@react-three/drei";
+//@ts-ignore
+import baffle from "baffle";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<color attach='background' args={["#333333"]} />
+			<ambientLight intensity={0.2} />
+			<spotLight
+				position={[0, 25, 0]}
+				angle={1.3}
+				penumbra={1}
+				castShadow
+				intensity={2}
+				shadow-bias={-0.0001}
+			/>
+			<Environment preset='warehouse' />
+		</>
+	);
 }
 
 export default App;
