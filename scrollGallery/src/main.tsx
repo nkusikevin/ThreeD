c78ee8f0269f06b66934ed633ae82ee3f0dccf06
nkusikevin <nkusikvnhart@gmail.com>
2023-05-12 15:15:27 +0200

    init scroll

--- scrollGallery/src/main.tsx
+++ scrollGallery/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
+			<App />
+		</Canvas>
+	</React.StrictMode>
+);
