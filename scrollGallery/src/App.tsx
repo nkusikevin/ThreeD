278beb9bdae35ef88c404a4d63ddad70dd180a08
nkusikevin <nkusikvnhart@gmail.com>
2023-05-14 15:50:18 +0200

    adding images

--- scrollGallery/src/App.tsx
+++ scrollGallery/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { ScrollControls, Scroll } from "@react-three/drei";
+import Pages from "./Components/Pages";
+import { Suspense } from "react";
+import { Preload } from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Suspense fallback={null}>
+				<ScrollControls infinite horizontal damping={4} pages={4} distance={1}>
+					<Scroll>
+						<Pages />
+					</Scroll>
+					<Scroll html>
+						<h1 style={{ position: "absolute", top: "20vh", left: "-75vw" }}>
+							home
+						</h1>
+						<h1 style={{ position: "absolute", top: "20vh", left: "25vw" }}>
+							to
+						</h1>
+						<h1 style={{ position: "absolute", top: "20vh", left: "125vw" }}>
+							be
+						</h1>
+						<h1 style={{ position: "absolute", top: "20vh", left: "225vw" }}>
+							home
+						</h1>
+						<h1 style={{ position: "absolute", top: "20vh", left: "325vw" }}>
+							to
+						</h1>
+						<h1 style={{ position: "absolute", top: "20vh", left: "425vw" }}>
+							be
+						</h1>
+					</Scroll>
+				</ScrollControls>
+				<Preload />
+			</Suspense>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ scrollGallery/src/Components/Pages.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useThree } from "@react-three/fiber";
+import Page from "./Page";
+
+function Pages() {
+	const { width } = useThree((state) => state.viewport);
+	return (
+		<>
+			{/* <Page
+				position={[-width * 1, 0, 0]}
+				urls={["/trip1.jpg", "/trip2.jpg", "/trip3.jpg"]}
+			/> */}
+			<Page
+				position={[width * 0, 0, 0]}
+				urls={["/img1.jpg", "/img2.jpg", "/img3.jpg"]}
+			/>
+			<Page
+				position={[width * 1, 0, 0]}
+				urls={["/img4.jpg", "/img5.jpg", "/img6.jpg"]}
+			/>
+			{/* <Page
+				position={[width * 2, 0, 0]}
+				urls={["/trip1.jpg", "/trip2.jpg", "/trip3.jpg"]}
+			/> */}
+			<Page
+				position={[width * 3, 0, 0]}
+				urls={["/img1.jpg", "/img2.jpg", "/img3.jpg"]}
+			/>
+			<Page
+				position={[width * 4, 0, 0]}
+				urls={["/img4.jpg", "/img5.jpg", "/img6.jpg"]}
+			/>
+		</>
+	);
+}
+
+export default Pages;
