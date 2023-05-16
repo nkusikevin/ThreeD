53158bfe7b44808c0af094127254a7aaa160c82b
nkusikevin <nkusikvnhart@gmail.com>
2023-05-17 00:41:07 +0200

    adding model

--- officeinterio/src/App.tsx
+++ officeinterio/src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import {
+	OrbitControls,
+	Bounds,
+	BakeShadows,
+	useHelper,
+} from "@react-three/drei";
+import Office from "./Office";
+import { useFrame } from "@react-three/fiber";
 
 function App() {
-  const [count, setCount] = useState(0)
+	// const planeRef = useRef<any>();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// useFrame(({ camera }) => {
+	// 	planeRef.current.position.copy(camera.position);
+	// });
+
+	// useHelper(planeRef, PlaneHelper);
+	return (
+		<>
+			<color attach='background' args={["#252530"]} />
+			<ambientLight intensity={0.01} />
+			<hemisphereLight intensity={0.125} color='#8040df' groundColor='red' />
+			<spotLight
+				castShadow
+				color='orange'
+				intensity={2}
+				position={[-50, 50, 40]}
+				angle={0.25}
+				penumbra={1}
+				shadow-mapSize={[128, 128]}
+				shadow-bias={0.00005}
+			/>
+
+			{/* <Bounds fit clip observe margin={1}>
+				<Office />
+			</Bounds>
+			<BakeShadows /> */}
+
+			<OrbitControls
+				makeDefault
+				minAzimuthAngle={0}
+				maxAzimuthAngle={Math.PI / 2}
+				minPolarAngle={Math.PI / 3}
+				maxPolarAngle={Math.PI / 3}
+				enableZoom={true}
+				enablePan={true}
+				zoomSpeed={0.3}
+			/>
+			<gridHelper
+				args={[1000, 200, "#151515", "#020202"]}
+				position={[0, -4, 0]}
+			/>
+			<mesh
+				scale={200}
+				rotation={[-Math.PI / 2, 0, 0]}
+				position={[0, -4, 0]}
+				receiveShadow>
+				<planeGeometry args={[1000, 1000, 1, 1]} />
+				<shadowMaterial transparent opacity={0.3} />
+			</mesh>
+		</>
+	);
 }
 
-export default App
+export default App;
--- officeinterio/src/main.tsx
+++ officeinterio/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<Canvas
+			orthographic
+			shadows
+			dpr={[1, 2]}
+			camera={{ position: [10, 10, 10], zoom: 10 }}>
+			<App />
+		</Canvas>
+	</React.StrictMode>
+);
