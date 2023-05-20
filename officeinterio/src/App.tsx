46cbd03a3381edabd04e397bfe993fd919bca970
nkusikevin <nkusikvnhart@gmail.com>
2023-05-20 20:24:49 +0200

    adding new model

--- officeinterio/src/App.tsx
+++ officeinterio/src/App.tsx
@@ -8,18 +8,24 @@ import {
 } from "@react-three/drei";
 import Office from "./Office";
 import { useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
 
 function App() {
-	// const planeRef = useRef<any>();
-
-	// useFrame(({ camera }) => {
-	// 	planeRef.current.position.copy(camera.position);
-	// });
-
-	// useHelper(planeRef, PlaneHelper);
+	
+	//change the background color
+	const { color } = useControls({
+		color: {
+			value: "#000000",
+			label: "Background Color",
+			min: 0,
+			max: 1,
+			step: 0.01,
+			color: true,
+		}, 
+	});
 	return (
 		<>
-			<color attach='background' args={["black"]} />
+			<color attach='background' args={[color]} />
 			<ambientLight intensity={0.01} />
 			<Environment preset='city' />
 			{/* <hemisphereLight intensity={0.125} color='#8040df' groundColor='red' /> */}
